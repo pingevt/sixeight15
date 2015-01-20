f1667c71a601a0521fde61ef7d00306a110fa677
Pete Inge <pete.inge@gmail.com>
2015-01-20 16:58:37 -0500

    - theme development
    Signed-off-by: Pete Inge <pete.inge@gmail.com>

--- drupal/sites/all/themes/developed/sixeight15_theme/template.php
+++ drupal/sites/all/themes/developed/sixeight15_theme/template.php
@@ -4,3 +4,27 @@
  * @file
  * template.php
  */
+
+
+/**
+ * Implements hook_preprocess_page().
+ */
+function sixeight15_theme_preprocess_page(&$vars) {
+  // Add in search form.
+  $search = drupal_get_form('search_form');
+  $vars['search'] = render($search);
+
+}
+
+function sixeight15_theme_bootstrap_search_form_wrapper($variables) {
+  $output = '<div class="input-group">';
+  $output .= $variables['element']['#children'];
+  $output .= '<span class="input-group-btn">';
+  $output .= '<button type="submit" class="btn btn-default">';
+
+  $output .= '<i class="fa fa-search"></i>';
+  $output .= '</button>';
+  $output .= '</span>';
+  $output .= '</div>';
+  return $output;
+}
