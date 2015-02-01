bcf19b73f86e64ab453eb7fd139c767decdcb066
Pete Inge <pete.inge@gmail.com>
2015-02-01 15:09:09 -0500

    - dashboard views for ad and blocks
    Signed-off-by: Pete Inge <pete.inge@gmail.com>

---
+++ sites/all/modules/custom/sixeight_admin/templates/sixeight-admin-chart.tpl.php
@@ -0,0 +1,21 @@
+<?php
+dpm($element);
+
+?>
+
+
+<table>
+  <tr>
+    <th width="100px">Node</th><th><?php print $dates; ?></th>
+  </tr>
+<?php
+  foreach ($element['#items'] as $item) {
+?>
+  <tr>
+    <td><?php print l($item['node']->title, 'node/' . $item['node']->nid); ?></td>
+    <td><?php print $item['graph']; ?></td>
+  </tr>
+<?php
+  }
+?>
+</table>
---
+++ sites/all/modules/custom/sixeight_admin/templates/sixeight-admin-progress-bar.tpl.php
@@ -0,0 +1,9 @@
+<?php
+dpm($element);
+?>
+
+<div class="progress">
+  <div class="<?php print $classes; ?>" style="width: <?php print $element['#end_percentage']; ?>%; left: <?php print $element['#start_percentage']; ?>%;">
+    <span class="sr-only">35% Complete (success)</span>
+  </div>
+</div>
