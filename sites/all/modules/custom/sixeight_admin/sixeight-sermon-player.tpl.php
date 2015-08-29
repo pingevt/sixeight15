dfd170a72ef9b590fa64780c1a95ddf03dc7f981
Pete Inge <pete.inge@gmail.com>
2015-08-29 16:45:49 -0400

    - removed player button for sermons
    Signed-off-by: Pete Inge <pete.inge@gmail.com>

--- sites/all/modules/custom/sixeight_admin/sixeight-sermon-player.tpl.php
+++ sites/all/modules/custom/sixeight_admin/sixeight-sermon-player.tpl.php
@@ -2,4 +2,4 @@
 //dpm(get_defined_vars());
 ?>
 
-<span class="file"><?php print $icon; ?> <?php print $text; ?> <?php print $download_link; ?> <?php print $stream_link; ?></span>
+<span class="file"><?php print $icon; ?> <?php print $text; ?> <?php print $download_link; ?> <?php //print $stream_link; ?></span>
